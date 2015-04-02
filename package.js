b302b5bf8a135de916dc61a6009d7d9d8ffbe262
Sergiy Dybskiy <sdybskiy@gmail.com>
2015-04-02 17:30:00 -0400

    added package versions to dependencies

--- package.js
+++ package.js
@@ -13,9 +13,9 @@ Package.describe({
 Package.onUse(function(api) {
   api.use('accounts-base', ['client', 'server']);
   // Export Accounts (etc) to packages using this one.
-  api.imply('accounts-base', ['client', 'server']);
-  api.use('accounts-oauth', ['client', 'server']);
-  api.use('416serg:uber', ['client', 'server']);
+  api.imply('accounts-base@1.2.0', ['client', 'server']);
+  api.use('accounts-oauth@1.1.5', ['client', 'server']);
+  api.use('416serg:uber@0.0.1', ['client', 'server']);
 
   api.addFiles('uber_login_button.css', 'client');
 
